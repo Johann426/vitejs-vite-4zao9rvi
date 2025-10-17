f579fe4fd9f3c81733a6fc5f8b308d6432f618e3
Johann426 <johannd0426@gmail.com>
2025-10-17 09:26:44 +0900

    added header to table

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,17 @@ import "./App.css";
 import Table from "./UITable.tsx";
 
 function App() {
+  const header = ["A", "B", "C"];
+
+  const data = [
+    ["a1", "b1", "b1"],
+    ["a2", "b2", "b2"],
+    ["a3", "b3", "b3"],
+  ];
+
   return (
     <>
-      <Table />
+      <Table header={header} data={data} />
     </>
   );
 }
--- src/UITable.tsx
+++ src/UITable.tsx
@@ -1,51 +1,60 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 
-type Cell = {
-  i: number;
-  j: number;
+interface Head {
+  val: string;
+  onPointerDown: () => void;
+}
+
+function Th({ val, onPointerDown }: Head) {
+  return <th onPointerDown={onPointerDown}>{val}</th>;
+}
+
+interface Cell {
+  row: number;
+  col: number;
   val: string;
   bool: boolean;
   className: string;
-  onDoubleClick: () => void;
-  onChange: (row: number, j: number, val: string) => void;
   onBlur: () => void;
+  onPointerDown: () => void;
+  onPointerEnter: () => void;
+  onDoubleClick: () => void;
+  onChange: (row: number, col: number, val: string) => void;
   onKeyDown: (
     i: number,
     j: number,
     e: React.KeyboardEvent<HTMLInputElement>
   ) => void;
-  onPointerDown: () => void;
-  onPointerEnter: () => void;
-};
+}
 
 function Td({
-  i,
-  j,
+  row: i,
+  col: j,
   val,
   bool,
   className,
-  onDoubleClick,
-  onChange,
   onBlur,
-  onKeyDown,
   onPointerDown,
   onPointerEnter,
+  onDoubleClick,
+  onChange,
+  onKeyDown,
 }: Cell) {
   return (
     <td
       className={className}
-      onDoubleClick={onDoubleClick}
       onPointerDown={onPointerDown}
       onPointerEnter={onPointerEnter}
-      onClick={(e) => e.currentTarget.focus()}
+      onDoubleClick={onDoubleClick}
+      // onClick={(e) => e.currentTarget.focus()}
     >
-      {bool ? ( // render input element inside <td> if
-        <input
+      {bool ? (
+        <input // render input element inside <td>
           autoFocus
           onFocus={(e) => e.target.select()}
           value={val}
-          onChange={(e) => onChange(i, j, e.target.value)}
           onBlur={onBlur}
+          onChange={(e) => onChange(i, j, e.target.value)}
           onKeyDown={(e) => onKeyDown(i, j, e)}
         />
       ) : (
@@ -55,7 +64,12 @@ function Td({
   );
 }
 
-export default function Table() {
+interface TableData {
+  header: string[];
+  data: string[][];
+}
+
+export default function Table({ header, data }: TableData) {
   type CellCoords = { row: number; col: number } | null;
 
   const [coord, setCoord] = useState<CellCoords>(null); // Coordinates of the cell being edited
@@ -66,145 +80,21 @@ export default function Table() {
 
   const [drag, setDrag] = useState<boolean>(false);
 
-  const [data, setData] = useState<string[][]>([
-    ["A1", "B1", "C1"],
-    ["A2", "B2", "C2"],
-    ["A3", "B3", "C3"],
-  ]);
+  const [head, setHead] = useState<string[]>(header);
+
+  const [rows, setRows] = useState<string[][]>(data);
 
   type CellRange = { imin: number; imax: number; jmin: number; jmax: number };
 
-  const getRange = useCallback(
-    (lead: CellCoords, tail: CellCoords): CellRange | null => {
-      if (!lead || !tail) return null;
-      return {
-        imin: Math.min(lead.row, tail.row),
-        imax: Math.max(lead.row, tail.row),
-        jmin: Math.min(lead.col, tail.col),
-        jmax: Math.max(lead.col, tail.col),
-      };
-    },
-    []
-  );
-
-  const onDoubleClick = (i: number, j: number): void => {
-    setCoord({ row: i, col: j });
-  };
-
-  const onKeyDown = (
-    i: number,
-    j: number,
-    e: React.KeyboardEvent<HTMLInputElement>
-  ): void => {
-    switch (e.key) {
-      case "Enter":
-      case "ArrowDown":
-        setCoord({ row: i + 1, col: j });
-        e.preventDefault();
-        break;
-      case "ArrowUp":
-        setCoord({ row: i - 1, col: j });
-        e.preventDefault();
-        break;
-      case "ArrowLeft":
-        setCoord({ row: i, col: j - 1 });
-        e.preventDefault();
-        break;
-      case "ArrowRight":
-        setCoord({ row: i, col: j + 1 });
-        e.preventDefault();
-        break;
-    }
-  };
-
-  const onChange = (i: number, j: number, val: string): void => {
-    const rows = data.map((row) => [...row]);
-    rows[i][j] = val;
-    setData(rows);
-  };
-
-  useEffect(() => {
-    const onCopy = (): void => {
-      const range = getRange(lead, tail);
-      if (!range) return;
-      const { imin, imax, jmin, jmax } = range;
-      const copied: string[] = [];
-      for (let i = imin; i <= imax; i++) {
-        const row: string[] = [];
-        for (let j = jmin; j <= jmax; j++) {
-          row.push(data[i][j]);
-        }
-        copied.push(row.join("\t"));
-      }
-      const clipboardText = copied.join("\n");
-      navigator.clipboard.writeText(clipboardText).then(() => {
-        console.log("Copied to clipboard:");
-        console.log(clipboardText);
-      });
+  const getRange = (lead: CellCoords, tail: CellCoords): CellRange | null => {
+    if (!lead || !tail) return null;
+    return {
+      imin: Math.min(lead.row, tail.row),
+      imax: Math.max(lead.row, tail.row),
+      jmin: Math.min(lead.col, tail.col),
+      jmax: Math.max(lead.col, tail.col),
     };
-
-    const onPaste = async () => {
-      if (!lead) return;
-
-      try {
-        const txt = await navigator.clipboard.readText();
-        const arr = txt.split("\n").map((row) => row.split("\t"));
-        const last = arr[arr.length - 1];
-        if (last.length === 1 && last[0] === "") {
-          arr.pop(); // remove if the last element is empty
-        }
-
-        const dat = [...data];
-
-        for (let i = 0; i < arr.length; i++) {
-          for (let j = 0; j < arr[i].length; j++) {
-            const row = lead.row + i;
-            const col = lead.col + j;
-            if (row < data.length && col < data[0].length) {
-              dat[row][col] = arr[i][j];
-            }
-          }
-        }
-
-        setData(dat);
-      } catch (err) {
-        console.error("Failed to read clipboard:", err);
-      }
-    };
-
-    const handleKeyDown = (e: KeyboardEvent) => {
-      const isEditing = document.activeElement?.tagName === "INPUT";
-
-      if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === "c") {
-        e.preventDefault();
-        onCopy();
-      }
-      if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === "v") {
-        e.preventDefault();
-        onPaste();
-      }
-      if (e.key === "Escape") {
-        setLead(null);
-      }
-      if (e.key === "Delete" && !isEditing) {
-        e.preventDefault();
-        const range = getRange(lead, tail);
-        if (!range) return;
-
-        const { imin, imax, jmin, jmax } = range;
-        const dat = [...data];
-
-        for (let i = imin; i <= imax; i++) {
-          for (let j = jmin; j <= jmax; j++) {
-            dat[i][j] = "";
-          }
-        }
-        setData(dat);
-      }
-    };
-    window.addEventListener("keydown", handleKeyDown);
-    return () => window.removeEventListener("keydown", handleKeyDown);
-  }, [lead, tail, data, getRange]);
+  };
 
   const onPointerDown = (i: number, j: number): void => {
     setDrag(true);
@@ -221,6 +111,16 @@ export default function Table() {
     setDrag(false);
   };
 
+  const onDoubleClick = (i: number, j: number): void => {
+    setCoord({ row: i, col: j });
+  };
+
+  const onChange = (i: number, j: number, val: string): void => {
+    const dat = rows.map((row) => [...row]);
+    dat[i][j] = val;
+    setRows(dat);
+  };
+
   const isCellInRange = (i: number, j: number): boolean => {
     const range = getRange(lead, tail);
     if (!range) return false;
@@ -228,25 +128,151 @@ export default function Table() {
     return i >= imin && i <= imax && j >= jmin && j <= jmax;
   };
 
+  const onCopy = (): void => {
+    const range = getRange(lead, tail);
+    if (!range) return;
+    const { imin, imax, jmin, jmax } = range;
+    const copied: string[] = [];
+    for (let i = imin; i <= imax; i++) {
+      const row: string[] = [];
+      for (let j = jmin; j <= jmax; j++) {
+        row.push(rows[i][j]);
+      }
+      copied.push(row.join("\t"));
+    }
+    const clipboardText = copied.join("\n");
+    navigator.clipboard.writeText(clipboardText).then(() => {
+      console.log("Copied to clipboard:");
+      console.log(clipboardText);
+    });
+  };
+
+  const onPaste = async () => {
+    if (!lead) return;
+
+    try {
+      const txt = await navigator.clipboard.readText();
+      const arr = txt.split("\n").map((row) => row.split("\t"));
+      const last = arr[arr.length - 1];
+      if (last.length === 1 && last[0] === "") {
+        arr.pop(); // remove if the last element is empty
+      }
+
+      const dat = [...rows];
+
+      for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+          const row = lead.row + i;
+          const col = lead.col + j;
+          if (row < rows.length && col < rows[0].length) {
+            dat[row][col] = arr[i][j];
+          }
+        }
+      }
+
+      setRows(dat);
+    } catch (err) {
+      console.error("Failed to read clipboard:", err);
+    }
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLTableElement>) => {
+    if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === "c") {
+      e.preventDefault();
+      onCopy();
+    }
+    if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === "v") {
+      e.preventDefault();
+      onPaste();
+    }
+    if (e.key === "Escape") {
+      setLead(null);
+    }
+    if (e.key === "Delete" && !coord) {
+      e.preventDefault();
+      const range = getRange(lead, tail);
+      if (!range) return;
+
+      const { imin, imax, jmin, jmax } = range;
+      const dat = [...rows];
+
+      for (let i = imin; i <= imax; i++) {
+        for (let j = jmin; j <= jmax; j++) {
+          dat[i][j] = "";
+        }
+      }
+      setRows(dat);
+    }
+  };
+
+  const onKeyDownCell = (
+    i: number,
+    j: number,
+    e: React.KeyboardEvent<HTMLInputElement>
+  ): void => {
+    const deSelect = () => {
+      setLead(null);
+      setTail(null);
+      e.preventDefault();
+    };
+
+    switch (e.key) {
+      case "Enter":
+      case "ArrowDown":
+        deSelect();
+        setCoord({ row: i + 1, col: j });
+        break;
+      case "ArrowUp":
+        deSelect();
+        setCoord({ row: i - 1, col: j });
+        break;
+      case "ArrowLeft":
+        deSelect();
+        setCoord({ row: i, col: j - 1 });
+        break;
+      case "ArrowRight":
+        deSelect();
+        setCoord({ row: i, col: j + 1 });
+        break;
+    }
+  };
+
+  const onPointerDownHeader = (j: number): void => {
+    setLead({ row: 0, col: j });
+    setTail({ row: data.length, col: j });
+    console.log(j);
+  };
+
   return (
-    <table onPointerUp={onPointerUp}>
+    <table tabIndex={0} onPointerUp={onPointerUp} onKeyDown={onKeyDown}>
+      <thead>
+        <tr>
+          {head.map((v, j) => (
+            <Th
+              key={`col${j}`}
+              val={v}
+              onPointerDown={() => onPointerDownHeader(j)}
+            />
+          ))}
+        </tr>
+      </thead>
       <tbody>
-        {data.map((arr, i) => (
+        {rows.map((arr, i) => (
           <tr key={i}>
             {arr.map((v, j) => (
               <Td
                 key={j}
-                i={i}
-                j={j}
+                row={i}
+                col={j}
                 val={v}
                 bool={coord?.row == i && coord?.col == j}
                 className={isCellInRange(i, j) ? "selected" : ""}
-                onDoubleClick={() => onDoubleClick(i, j)}
-                onChange={onChange}
                 onBlur={() => setCoord(null)}
-                onKeyDown={onKeyDown}
                 onPointerDown={() => onPointerDown(i, j)}
                 onPointerEnter={() => onPointerEnter(i, j)}
+                onDoubleClick={() => onDoubleClick(i, j)}
+                onChange={onChange}
+                onKeyDown={onKeyDownCell}
               />
             ))}
           </tr>
