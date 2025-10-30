b376299558fbdba5c4f436f2c95774272e5636f7
Johann426 <johannd0426@gmail.com>
2025-10-30 18:01:04 +0900

    tmp

--- src/TreeView.tsx
+++ src/TreeView.tsx
@@ -3,9 +3,28 @@ import { useState, useEffect } from "react";
 interface ItemData {
   label: string;
   obj: object;
+  onClick: () => void;
+  onPointerOver: () => void;
+  onPointerLeave: () => void;
+  onDragStart: () => void;
+  onDragOver: () => void;
+  onDragLeave: () => void;
+  onDragEnd: () => void;
+  onDrop: () => void;
 }
 
-function Item({ label, obj }: ItemData) {
+function Item({
+  label,
+  obj,
+  onClick,
+  onPointerOver,
+  onPointerLeave,
+  onDragStart,
+  onDragOver,
+  onDragLeave,
+  onDragEnd,
+  onDrop,
+}: ItemData) {
   return (
     <li
       draggable
@@ -23,9 +42,27 @@ interface GroupData {
   bool: boolean; // boolean indicating folded state
   group: GroupData[] | null; // list of sub group
   items: ItemData[] | null; // lisst of items
+  onClick: () => void;
+  onDragStart: () => void;
+  onDragOver: () => void;
+  onDragLeave: () => void;
+  onDragEnd: () => void;
+  onDrop: () => void;
 }
 
-function Group({ label, obj, bool, group, items }: GroupData) {
+function Group({
+  label,
+  obj,
+  bool,
+  group,
+  items,
+  onClick,
+  onDragStart,
+  onDragOver,
+  onDragLeave,
+  onDragEnd,
+  onDrop,
+}: GroupData) {
   return (
     <li>
       <span>{label}</span>
@@ -129,6 +166,71 @@ export default function TreeView({ groupList, itemList }: TreeData) {
     setCntxt(true);
     setCoord({ x: e.clientX, y: e.clientY });
   };
+
+  const onClick = (e) => {
+    // editor.select( mesh );
+    e.target.className = "selected";
+  };
+
+  const onPointerOver = () => {
+    // const mesh = scope.map.get( event.target );
+    // scope.savedColor = mesh.material.color.getStyle();
+    // mesh.material.color.setRGB( 255, 255, 255 );
+  };
+
+  const onPointerLeave = () => {
+    // const mesh = scope.map.get( event.target );
+    // mesh.material.color.set( scope.savedColor );
+  };
+
+  const onDragStart = (event) => {
+    // scope.dragged = event.target;
+    // event.target.classList.add("dragging");
+  };
+
+  const onDragEnd = (event) => {
+    // scope.dragged = 0;
+    // event.target.classList.remove("dragging");
+  };
+
+  const onDragLeave = () => {
+    // if (scope.dragged === event.target) return;
+    // event.target.classList.remove("dragTop", "dragBottom", "dragOver");
+  };
+
+  const onDragOver = () => {
+    // event.preventDefault(); // prevent default to allow drop event
+    // event.stopPropagation(); // Prevent event from reaching child nodes
+    // if (scope.dragged === event.target) return;
+    // const area = event.offsetY / event.target.clientHeight;
+    // if (area <= 0.5) {
+    //   event.target.classList.remove("dragBottom");
+    //   event.target.classList.add("dragTop");
+    // } else if (area > 0.5) {
+    //   event.target.classList.remove("dragTop");
+    //   event.target.classList.add("dragBottom");
+    // }
+  };
+
+  const onDrop = () => {
+    // event.preventDefault();
+    // const ul = event.target.parentElement;
+    // const li =
+    //   scope.dragged.tagName == "LI"
+    //     ? scope.dragged
+    //     : scope.dragged.parentElement;
+    // if (event.target === scope.dragged) return;
+    // const area = event.offsetY / this.clientHeight;
+    // // move to new position (insertElement does not create a copy)
+    // event.target.insertAdjacentElement(
+    //   area < 0.5 ? "beforebegin" : "afterend",
+    //   li
+    // );
+    // event.target.classList.remove("dragTop", "dragBottom");
+  };
+
+  // function onDragOver(event) {}
+  // function onDropFolder(event) {}
 
   const onNewItem = () => {
     const newItems = items?.slice();
